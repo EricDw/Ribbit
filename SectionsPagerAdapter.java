1a3431e9d92d81c1f255699306c1a0cd90d9d052
Eric <dewildte@gmail.com>
2014-08-20 00:22:48 -0400

    Added color to the title text to match the theme and added icons to the two the inbox and friends tabs.

--- SectionsPagerAdapter.java
+++ SectionsPagerAdapter.java
@@ -5,6 +5,9 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import com.teamtreehouse.ribbit.ui.FriendsFragment;
+import com.teamtreehouse.ribbit.ui.InboxFragment;
+
 import java.util.Locale;
 
 /**
